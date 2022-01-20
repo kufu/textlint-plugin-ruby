de91c557c1685e0fdd6e6ab3d6ca9075b16f7ce4
alpaca-tc <alpaca-tc@alpaca.tc>
2022-01-20 12:14:36 +0900

    Add configuration

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import type { TextlintPluginCreator } from "@textlint/types";
 import { RubyProcessor } from "./RubyProcessor";
 
-const creator: TextlintPluginCreator = {
-  Processor: RubyProcessor,
-};
+  const creator: TextlintPluginCreator = {
+    Processor: RubyProcessor,
+  };
 
 export default creator;
